0b901b562bc944dc70113bd5e98a978461c2b6de
Daniels1610 <danielagraz1610@hotmail.com>
2025-01-13 00:08:06 -0800

    Add asynchronous processing on form submission and display results on same page

--- app/main/routes.py
+++ app/main/routes.py
@@ -14,18 +14,15 @@ def init():
 def index():
     status_code = 200
     if (request.method == 'POST'):
-        name = request.form['monomer']
-        smile = request.form['smiles']
+        data = request.get_json()
+        mol = Molecule(data['smiles'], data['monomer'])
 
-        mol = Molecule(smile, name)
-
-        if (mol.isValid()):
+        if (mol.isValid() and len(data['monomer']) > 0):
             image_io = BytesIO()
             mol.diagram.save(image_io, 'png')
             mol.set_diagram('data:image/png;base64,' + b64encode(image_io.getvalue()).decode('ascii'))
-            return render_template("results.html", mol=mol.to_dict()), 200
+            return jsonify(mol.to_dict())
         else:
-            flash("Invalid input. Enter your SMILES again")
             status_code = 400
         
     return render_template("index.html"), status_code
--- app/static/index.js
+++ app/static/index.js
@@ -1,12 +1,13 @@
 const monomerInput = document.getElementById('monomer_input');
 const smilesInput = document.getElementById('smiles_input');
 const flashAlert = document.querySelector('.flash');
+const submitButton = document.getElementById('submit_btn');
+const molResults = document.querySelector('.mol-results');
+
+/// INPUT FORM RESTRICTIONS ///
 
 // Prevent Copy-Pase on Monomer Name field
-window.onload = (e) => {
-    monomerInput.onpaste = e => e.preventDefault();
-    removeFlash();
-}
+monomerInput.onpaste = e => e.preventDefault();
 
 // Allows only alphanumeric values on monomer name field
 monomerInput.addEventListener('beforeinput', (e) => {
@@ -20,10 +21,58 @@ monomerInput.addEventListener('beforeinput', (e) => {
         return;
 });
 
+// Remove flash message about Invalid Smiles after 3 seconds
 const removeFlash = () => {
     setTimeout(() => {
-        flashAlert.remove();
+        flashAlert.style.display = "none";
     }, 3000)
 }
 
+/// ASYNCHRONOUS LOADING ///
 
+// When user submits form, sends a POST request to the Flask server with values
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    chemicalPOST();
+});
+
+async function chemicalPOST () {
+    options = {
+        method: "POST",
+        body: JSON.stringify({monomer: monomerInput.value, smiles: smilesInput.value}),
+        headers: {
+            'Content-Type' : 'application/json'
+        }
+    }
+
+    try {
+        const response = await fetch("/chemical", options)
+
+        if (response.status == 400) {
+            flashAlert.style.display = "block";
+            removeFlash();
+        }
+
+        const data = await response.json();
+
+        // Set Molecule properties
+        let molInfo = [data.name, 
+            data.diagram, 
+            `<b>Molecule Weight:</b> ${data.weight}`, 
+            `<b>Molecule Group:</b> ${data.group}`];
+
+        let molResultsChildren = molResults.children;
+        for (i = 0; i < molResultsChildren.length; i++) {
+            if (molResultsChildren[i].className === "mol-image") {
+                molResultsChildren[i].src = molInfo[i];
+            } else {
+                molResultsChildren[i].innerHTML = molInfo[i];
+            }
+            
+            molResultsChildren[i].style.display = "block"
+        }
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
