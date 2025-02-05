668e9ad05a67f3a67d2aa276938586f9aaec8197
Daniels1610 <danielagraz1610@hotmail.com>
2025-02-04 22:27:51 -0600

    Add badRequestError toast message. Follow Separation of Concerns(SoC) remove HTML or JS code that changes an element style

--- app/static/index.js
+++ app/static/index.js
@@ -1,10 +1,21 @@
+// SmiViewer Form
 const monomerInput = document.getElementById('monomer_input');
 const smilesInput = document.getElementById('smiles_input');
-const flashAlert = document.querySelector('.flash');
 const submitButton = document.getElementById('submit_btn');
-const molResults = document.querySelector('.mol-results');
 
-/// INPUT FORM RESTRICTIONS ///
+// BadRequestError elements
+const badRequestDiv = document.createElement("div");
+const badRequestSpan = document.createElement("span");
+const badRequestText = "⚠️ Invalid input. The SMILES or Monomer Name is incorrect.";
+badRequestDiv.appendChild(badRequestSpan);
+
+// MoleculeResults elements
+const molResults = document.querySelector(".mol-results");
+const molName = document.querySelector(".mol-name");
+const molImage = document.createElement("img");
+
+
+/** INPUT FORM RESTRICTIONS */
 
 // Prevent Copy-Pase on Monomer Name field
 monomerInput.onpaste = e => e.preventDefault();
@@ -24,11 +35,12 @@ monomerInput.addEventListener('beforeinput', (e) => {
 // Remove flash message about Invalid Smiles after 3 seconds
 const removeFlash = () => {
     setTimeout(() => {
-        flashAlert.style.display = "none";
+        badRequestDiv.remove();
     }, 3000)
 }
 
-/// ASYNCHRONOUS LOADING ///
+
+/** ASYNCHRONOUS LOADING */  
 
 // When user submits form, sends a POST request to the Flask server with values
 submitButton.addEventListener('click', (e) => {
@@ -40,37 +52,36 @@ async function chemicalPOST () {
     options = {
         method: "POST",
         body: JSON.stringify({monomer: monomerInput.value, smiles: smilesInput.value}),
-        headers: {
-            'Content-Type' : 'application/json'
-        }
+        headers: {'Content-Type' : 'application/json'}
     }
 
     try {
         const response = await fetch("/chemical", options)
 
         if (response.status == 400) {
-            flashAlert.style.display = "block";
+            badRequestSpan.innerHTML = badRequestText;
+            badRequestDiv.classList.add("flash", "alert","alert-warning");
+            document.getElementById("smiles_field").after(badRequestDiv);
             removeFlash();
         }
 
         const data = await response.json();
 
         // Set Molecule properties
-        let molInfo = [data.name, 
-            data.diagram, 
+        let molInfo = [data.name,  
             `<b>Molecule Weight:</b> ${data.weight}`, 
             `<b>Molecule Group:</b> ${data.group}`];
 
         let molResultsChildren = molResults.children;
-        for (i = 0; i < molResultsChildren.length; i++) {
-            if (molResultsChildren[i].className === "mol-image") {
-                molResultsChildren[i].src = molInfo[i];
-            } else {
+        for (i = 0; i < molResultsChildren.length-1; i++) {
                 molResultsChildren[i].innerHTML = molInfo[i];
             }
-            
-            molResultsChildren[i].style.display = "block"
-        }
+        
+        // Set Image attributes
+        molImage.src = data.diagram;
+        molImage.classList.add("mol-image")
+        molName.after(molImage);
+        
     }
     catch(error) {
         console.error(error);
