cdce6b49426aa56c51a14ae2dc4d8fea5090b91f
Daniels1610 <danielagraz1610@hotmail.com>
2025-02-04 20:28:23 -0600

    Fix defensive code using len(x) > 0

--- app/main/routes.py
+++ app/main/routes.py
@@ -17,7 +17,7 @@ def index():
         data = request.get_json()
         mol = Molecule(data['smiles'], data['monomer'])
 
-        if (mol.isValid() and len(data['monomer']) > 0):
+        if (mol.isValid() and data.get('monomer')):
             image_io = BytesIO()
             mol.diagram.save(image_io, 'png')
             mol.set_diagram('data:image/png;base64,' + b64encode(image_io.getvalue()).decode('ascii'))
